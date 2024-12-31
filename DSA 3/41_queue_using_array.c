47d57d87abfb94ebc5db6f9f67cb3e783c30c3d0
Wazmi Ali <113008194+wazmiali@users.noreply.github.com>
2024-12-31 19:44:47 +0530

    Queue using array in c
    Queue using array in c.

---
+++ DSA 3/41_queue_using_array.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct queue {
+      int size;
+      int f;
+      int r;
+      int * arr;
+};
+
+int isEmpty(struct queue *q){
+      if(q->r==q->f){
+            return 1;
+      }
+      return 0;
+}
+
+int isFull(struct queue *q){
+      if(q->r==q->size-1){
+            return 1;
+      }
+      return 0;
+}
+
+void enqueue(struct queue*q, int val){
+      if(isFull(q)){
+            printf("This Queue is Full.");
+      }
+      else{
+            q->r++;
+            q->arr[q->r]= val;
+      }
+}
+int dequeue(struct queue*q){
+      int a = -1;
+      if(isEmpty(q)){
+            printf("This Queue is Full.");
+      }
+      else{
+            q->f++;
+            a = q->arr[q->f];
+      }
+}
+
+int main(){
+      struct queue q;
+      q.size = 100;
+      q.f = q.r = -1;
+      q.arr = (int*)malloc(q.size*sizeof(int));
+
+      
+      // Enqueue few elements
+      enqueue(&q, 12);
+      enqueue(&q, 15);
+      enqueue(&q, 1);
+      printf("Dequeue element %d\n", dequeue(&q));
+      printf("Dequeue element %d\n", dequeue(&q));
+
+      if(isEmpty(&q)){
+            printf("Queue is Empty.\n");
+      }
+      if(isFull(&q)){
+            printf("Queue is Full.\n");
+      }
+      
+      return 0; 
+} 
