977fb04aaf96779a5f6a4af988e19f90d1405e04
Wazmi Ali <113008194+wazmiali@users.noreply.github.com>
2025-02-23 17:33:50 +0530

    Add files via upload

---
+++ DSA 3/69_Inorder_traversal_B_tree.c
@@ -0,0 +1,81 @@
+// Binary tree InOrder traversal:
+
+#include <stdio.h>
+#include <stdlib.h> 
+
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+struct node *createNode(int data)
+{
+    struct node *n = (struct node *)malloc(sizeof(struct node)); // Allocating memory
+    n->data = data;
+    n->left = NULL;
+    n->right = NULL;
+    return n;
+}
+
+void preOrder(struct node *root)
+{
+    if (root != NULL)
+    {
+        printf("%d ", root->data);
+        preOrder(root->left);
+        preOrder(root->right);
+    }
+}
+
+void postOrder(struct node *root)
+{
+    if (root != NULL)
+    {
+        postOrder(root->left);
+        postOrder(root->right);
+        printf("%d ", root->data);
+    }
+}
+
+void inOrder(struct node *root)
+{
+    if (root != NULL)
+    {
+        inOrder(root->left);
+        printf("%d ", root->data);
+        inOrder(root->right);
+    }
+}
+
+int main()
+{
+    // Constructing the root node
+    struct node *p = createNode(4);
+    struct node *p1 = createNode(1);
+    struct node *p2 = createNode(6);
+    struct node *p3 = createNode(5);
+    struct node *p4 = createNode(2);
+
+    // Constructing the tree correctly:
+    //        4
+    //       /  \
+    //      1    6
+    //     / \
+    //    5   2
+
+    p->left = p1;
+    p->right = p2;
+    p1->left = p3;
+    p1->right = p4;
+
+//     preOrder(p);
+//     printf("\n");
+//     postOrder(p);
+
+      printf("\n");
+      inOrder(p);
+
+    return 0;
+}
