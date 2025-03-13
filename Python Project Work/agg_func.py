09b7847a907bfa4d4655823fa565f2c22bf49680
Wazmi Ali <113008194+wazmiali@users.noreply.github.com>
2025-03-13 23:01:58 +0530

    Add files via upload

---
+++ Python Project Work/agg_func.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+arr = np.array([10, 20, 30, 40, 50])
+
+print(np.sum(arr))
+print(np.mean(arr))
+print(np.min(arr)) 
+print(np.max(arr))
+print(np.std(arr))
+print(np.var(arr))
