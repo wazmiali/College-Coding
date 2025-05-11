6d3f82b28fde1195109f0d721ca798a353f52d7a
Wazmi Ali <113008194+wazmiali@users.noreply.github.com>
2025-05-11 22:51:38 +0530

    Add files via upload

---
+++ DSA C++/Chapter_1 L1-L10/Lecture 19/Queue.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+      queue<string> q;
+
+      q.push("Wazmi");
+      q.push("Ali");
+      q.push("M");
+
+      cout<<"Size befor pop: "<<q.size()<<endl;
+
+      cout<<"First Element "<<q.front()<<endl;
+
+      q.pop();
+      cout<<"First Element "<<q.front()<<endl;
+
+      cout<<"Size after pop: "<<q.size()<<endl;
+
+
+
+}
