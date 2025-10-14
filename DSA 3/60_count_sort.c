6b7bb0305b51feb475ea84ffca15c0831be33f31
Wazmi Ali <113008194+wazmiali@users.noreply.github.com>
2025-10-14 18:30:48 +0530

    Update 60_count_sort.c

--- DSA 3/60_count_sort.c
+++ DSA 3/60_count_sort.c
@@ -43,7 +43,7 @@ void countSort(int *A, int n)
       {
             count[A[i]] = count[A[i]] + 1;
       }
-      i = 0; // counter for a count array
+      i = 0; // counter for a count arr
       j = 0; // counter for a given arr
 
       while (i <= max)
@@ -73,4 +73,5 @@ int main()
 
       return 0;
 }
+
 
